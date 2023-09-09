8378fe303cf4f78ef5e64114573cec3c5d1378a7
isaroro <isaroro20221@gmail.com>
2023-09-09 18:46:56 -0500

    inicializacion de proyecto

--- src/coponents/home/Home.jsx
+++ src/coponents/home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from 'react'
+import './home.css'
+import Presentacion from '../presentacion/Presentacion'
+import Conocenos from '../conocenos/Conocenos'
 
-const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+function Home() {
+  return (
+    <>
+    <div className='contenedor-home'>
+    <Presentacion />
+    </div>
+    <div className='contenedeor-conocenos'>
+    <Conocenos/>
+    </div>
+    
+    </>
+    
+  )
 }
 
-export default Home;
+export default Home
