a3981bf0e87f019063bc0410a0a8e1e5640debf5
isaroro <isaroro20221@gmail.com>
2023-09-09 18:25:08 -0500

    creacion de proyecto Ucamp 4

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Reservas from './coponents/reservas/Reservas';
+
+function App() {
+  return (
+    <div className="App">
+      <Reservas/>
+    </div>
+  );
+}
+
+export default App;
